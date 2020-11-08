d935b785caa47a7a567068dc08f4467d221cc3cf
zakariamosa <zakaria.mosa@iths.se>
2020-11-08 09:50:21 +0100

    laptop1

--- app/src/main/java/com/example/mygit/MainActivity.kt
+++ app/src/main/java/com/example/mygit/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.mygit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "laptop line 1", Toast.LENGTH_SHORT).show()
     }
 }
